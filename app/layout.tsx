63f6171edbb867d2cb0239c3c805ed6a4dc5755b
Umer Qayyum <muhammadumerqayyum@preesoft.com>
2026-03-28 21:47:56 +0500

    Update the favcion

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,23 +24,7 @@ export const metadata: Metadata = {
     description: 'Building scalable products that matter',
     type: 'website',
   },
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
+
 }
 
 export default function RootLayout({
